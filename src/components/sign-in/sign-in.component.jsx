a460f48c28555fd376c4a9ec20707e2b5d32b0ec
Lindsey <lindseyurquhart@tutanota.com>
2022-01-08 17:41:47 -0500

    added type button to Google Sign In button

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -63,8 +63,8 @@ class SignIn extends React.Component {
                     />
                     <div className="buttons">
                     <CustomButton type="submit">Sign In</CustomButton>
-                    <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
-                     Google Signin
+                    <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn>
+                     Google Sign In
                     </CustomButton>
                     </div>
                 </form>
