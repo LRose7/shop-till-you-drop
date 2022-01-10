c399361ab1181c0663b847e0b22c4052332f61b6
LRose7 <lindseyurquhart@tutanota.com>
2022-01-10 14:08:39 -0500

    added new logo image, made a sticky navbar

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import  Logo from '../../assets/crown.jpg';
+import  Logo from '../../assets/logo.png';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
